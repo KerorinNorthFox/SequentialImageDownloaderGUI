dc0d4b129bbdeda0350a692d707b58c6e33bd856
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2024-10-15 11:12:40 +0900

    configを扱うクラスを作成

---
+++ Models/Config/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Configuration;
+
+namespace MangaDownloader.Models.Config
+{
+    public class Config
+    {
+
+
+        public Config()
+        {
+            var a = ConfigurationManager.AppSettings;
+        }
+    }
+}
