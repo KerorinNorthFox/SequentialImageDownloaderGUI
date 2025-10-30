0d6624e31778b01f10192e74ac87fa95f0578ff1
kerorinnf <kerorinnf@gmail.com>
2025-10-30 13:22:45 +0900

    dllを読み込みIRuleを継承したクラスをインスタンス化して返すRuleActivatorクラスを実装

--- MangaDownloader.Desktop/Activator/RuleActivator.cs
+++ MangaDownloader.Desktop/Activator/RuleActivator.cs
@@ -1,20 +1,63 @@
-﻿using MangaDownloader.Desktop.Models;
+﻿using MangaDownloader.Rule;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace MangaDownloader.Desktop.Activator
 {
     public class RuleActivator
     {
-        private Config _config;
-
-        public RuleActivator(Config config)
+        public static List<IRule> LoadRules(string pluginDir)
         {
-            _config = config;
-        }
+            List<IRule> rules = new List<IRule>();
 
+            if (!Directory.Exists(pluginDir))
+            {
+                return rules;
+            }
+
+            foreach (var dllPath in Directory.GetFiles(pluginDir, "*.dll"))
+            {
+                Assembly asm;
+                try
+                {
+                    asm = Assembly.LoadFrom(dllPath);
+                    Debug.WriteLine($"{asm.GetName().Name} : assembly loaded.");
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine($"{e.Message} >> on Assembly.LoadFrom in LoadRules.");
+                    continue;
+                }
+
+                foreach (var type in asm.GetTypes())
+                {
+                    if (type == null)
+                    {
+                        continue;
+                    }
+
+                    if (type.GetInterfaces().Contains(typeof(IRule)))
+                    {
+                        try
+                        {
+                            var rule = (IRule)System.Activator.CreateInstance(type)!;
+                            rules.Add(rule);
+                            Debug.WriteLine($"{rule.Selector.Domain} : rule loaded.");
+                        }
+                        catch (Exception e)
+                        {
+                            Debug.WriteLine($"{e.Message} >> on Activator.CreateInstance in LoadRules");
+                            continue;
+                        }
+                    }
+                }
+            }
+
+            return rules;
+        }
     }
 }
